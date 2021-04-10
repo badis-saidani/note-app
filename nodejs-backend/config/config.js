e1f289ffc440723f72a790366a53bb4b992da5a4
badis-saidani <saidanii.badis@gmail.com>
2021-04-10 14:41:20 -0500

    hide config features

--- nodejs-backend/config/config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    'secret': 'mwa-project-secret-key',
-    'DB_CONNECTION' : 'mongodb+srv://badis:mwaproject@cluster0.8d71c.mongodb.net/mwa-project?retryWrites=true&w=majority'
-}
