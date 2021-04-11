9c43b21cbafe047c6a1b1f5c45a251cad1b90b46
badis-saidani <saidanii.badis@gmail.com>
2021-04-11 14:15:51 -0500

    structure the frontend and the home layout

--- angular-frontend/src/app/app.module.ts
+++ angular-frontend/src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(
+      [
+        {path: '', component: AppComponent},
+        {path: 'home', component: HomeComponent},
+        {path: 'reminders', loadChildren: () => import('./reminder/reminder.module').then(m=>m.ReminderModule)},
+      ]
+    ),
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
